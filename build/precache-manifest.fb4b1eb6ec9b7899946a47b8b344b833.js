178079c07b637b15280c41509025df011d8346f6
kentio <13550898+kentio@users.noreply.github.com>
2020-03-23 00:50:53 +0800

    two

---
+++ build/precache-manifest.fb4b1eb6ec9b7899946a47b8b344b833.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c363cb573d69ae04b77a1370e8143e71",
+    "url": "/react-todolist-demo/index.html"
+  },
+  {
+    "revision": "49e2a06c04a40cbaa389",
+    "url": "/react-todolist-demo/static/css/2.4b6e3ef2.chunk.css"
+  },
+  {
+    "revision": "49e2a06c04a40cbaa389",
+    "url": "/react-todolist-demo/static/js/2.9d67e25d.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/react-todolist-demo/static/js/2.9d67e25d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "63f3ee1adf1856eace70",
+    "url": "/react-todolist-demo/static/js/main.45ed8537.chunk.js"
+  },
+  {
+    "revision": "de608e1e5dbf915ffa2f",
+    "url": "/react-todolist-demo/static/js/runtime-main.7882924e.js"
+  }
+]);
