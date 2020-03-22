915ce83ec0b48374a7850f187ebf7a03599340e7
kentio <13550898+kentio@users.noreply.github.com>
2020-03-23 01:41:36 +0800

    Updates

---
+++ precache-manifest.f5a7e07e8d00f3ca7aa2fff991e480eb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e8c826497b6e2cdcf3b8a5a26a739cc2",
+    "url": "./index.html"
+  },
+  {
+    "revision": "49e2a06c04a40cbaa389",
+    "url": "./static/css/2.4b6e3ef2.chunk.css"
+  },
+  {
+    "revision": "49e2a06c04a40cbaa389",
+    "url": "./static/js/2.9d67e25d.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "./static/js/2.9d67e25d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "63f3ee1adf1856eace70",
+    "url": "./static/js/main.45ed8537.chunk.js"
+  },
+  {
+    "revision": "f8b6872298ba051c56f8",
+    "url": "./static/js/runtime-main.ed2d0ce8.js"
+  }
+]);
